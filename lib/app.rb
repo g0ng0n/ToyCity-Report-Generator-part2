6e0bc21a569a623db7c4af227be02a407b7cc45f
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2015-12-18 23:47:37 -0300

    added some boiler plate code to the app.rb file

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,3 @@
-require 'json'
-path = File.join(File.dirname(__FILE__), '../data/products.json')
-file = File.read(path)
-products_hash = JSON.parse(file)
 
 # Print "Sales Report" in ascii art
 
@@ -24,3 +20,18 @@ products_hash = JSON.parse(file)
 	# Count and print the number of the brand's toys we stock
 	# Calculate and print the average price of the brand's toys
 	# Calculate and print the total sales volume of all the brand's toys combined
+# Get path to products.json, read the file into a string,
+# and transform the string into a usable hash
+def setup_files
+    path = File.join(File.dirname(__FILE__), '../data/products.json')
+    file = File.read(path)
+    $products_hash = JSON.parse(file)
+    $report_file = File.new("report.txt", "w+")
+end
+
+def start
+  setup_files # load, read, parse, and create the files
+  create_report # create the report!
+end
+
+start # call start method to trigger report generation
