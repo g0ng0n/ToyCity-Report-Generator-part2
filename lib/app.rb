c6ee5373336ee4cac113b85302f12e36217ca53a
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2015-12-24 20:33:29 -0300

    added the Sales Report Header

--- lib/app.rb
+++ lib/app.rb
@@ -1,28 +1,5 @@
 require 'json'
-# Print "Sales Report" in ascii art
 
-# Print today's date
-
-# Print "Products" in ascii art
-
-# For each product in the data set:
-	# Print the name of the toy
-	# Print the retail price of the toy
-	# Calculate and print the total number of purchases
-  # Calcalate and print the total amount of sales
-  # Calculate and print the average price the toy sold for
-  # Calculate and print the average discount based off the average sales price
-
-# Print "Brands" in ascii art
-
-# For each brand in the data set:
-	# Print the name of the brand
-	# Count and print the number of the brand's toys we stock
-	# Calculate and print the average price of the brand's toys
-	# Calculate and print the total sales volume of all the brand's toys combined
-
-# Get path to products.json, read the file into a string,
-# and transform the string into a usable hash
 def setup_files
     path = File.join(File.dirname(__FILE__), '../data/products.json')
     file = File.read(path)
@@ -43,6 +20,15 @@ def print_date_time
     print_line_separator;
 end
 
+def print_sales_report_header
+
+    puts" ___    _    _     ___  ___   ___  ___  ___   ___   ___  _____ "
+    puts"/ __|  /_\\  | |   | __|/ __| | _ \\| __|| _ \\ / _ \\ | _ \\|_   _|"
+    puts"\\__ \\ / _ \\ | |__ | _| \\__ \\ |   /| _| |  _/| (_) ||   /  | |  "
+    puts"|___//_/ \\_\\|____||___||___/ |_|_\\|___||_|   \\___/ |_|_\\  |_|  "
+    puts"                                                               "  
+
+end
 def print_products_header
     puts "                     _            _       "
     puts "                    | |          | |      "
@@ -204,6 +190,7 @@ end
 
 def create_report
   $brands = [];
+  print_sales_report_header;
   print_date_time;
   print_products_header;
   create_products_report;
